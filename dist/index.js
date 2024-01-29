01291983d0bb532e21d0a93a3cddd3cc7e418055
Bluebberies <francisalexander000@gmail.com>
2024-01-29 03:14:30 -0500

    modified package.json

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const path = require("path");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+    // res.send("Welcome to Express & TypeScript Server");
+    res.sendFile(path.join(__dirname, "/../", "index.html"));
+});
+app.get("/test", (req, res) => {
+    res.send("Welcome to Express & TypeScript Server");
+});
+app.use(express_1.default.static(__dirname + "/../public"));
+app.listen(port, () => {
+    console.log(`Server is started at http://localhost:${port}`);
+});
