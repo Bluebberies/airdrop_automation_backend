4a3834a8eb2106f74934595f7e31cec6ca3e9f30
Bluebberies <francisalexander000@gmail.com>
2024-01-29 02:53:45 -0500

    added new route

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ const app: Application = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
-  //   res.send("Welcome to Express & TypeScript Server");
+    // res.send("Welcome to Express & TypeScript Server");
   res.sendFile(path.join(__dirname, "/../", "index.html"));
+});
+app.get("/test", (req: Request, res: Response) => {
+    res.send("Welcome to Express & TypeScript Server");
 });
 app.use(express.static(__dirname + "/../public"));
 
